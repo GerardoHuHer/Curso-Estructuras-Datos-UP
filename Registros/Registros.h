ecd0329ad649487f6d90557406137803a17748e5
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2024-02-07 11:18:39 -0600

    Registros/Struct

---
+++ Registros/Registros.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+/*¿Qué es un registro?
+Es un tipo de dato estructurado que consiste en un conjunto finito de elementos.
+Difiere de los arreglos en que sus elementos pueden ser de diferente tipo.
+A cada elemento del registro se le llama campo. Cada campo tiene un nombre único, por lo tanto, otra diferencia respecto a los arreglos es que a los campos se les llama por su nombre y no por su posición (índice) dentro de la estructura.
+A los registros también se les conoce como estructuras.*/
---
+++ Registros/source.cpp
@@ -0,0 +1,4 @@
+#include "Registros.h"
+int main() {
+    return 0;
+}
