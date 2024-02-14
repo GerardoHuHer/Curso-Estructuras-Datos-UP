c3a8c5280ce510db5d4cd8b52dfcff580b89985c
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2024-02-14 13:51:46 -0600

    Apuntadores

---
+++ Apuntadores/Apuntadores.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+
+/*
+Los apuntadores son variables que almacenan direcciones de memoria, también son llamadas variables de tipo apuntador o puntero.
+Difieren de las demás variables en que no alamacenan directamente los datos, sino la dirección en memoria en que se encuentra el dato.
+La utilidad del empleo de apuntadores se verá después de mostrar algunos ejemplos de su uso.
+La aritmética de apuntadores se refiere a las operaciones de suma y resta de constantes que se pueden realizar sobre las variables de tipo apuntador. Al sumar 1 a un apuntador, se obtiene la dirección de la siguiente localidad de memoria.
+*/
---
+++ Apuntadores/source.cpp
@@ -0,0 +1,8 @@
+#include "Apuntadores.h"
+int main() {
+    int num = 5;
+    int* direccion_num = &num;
+    std::cout << "Número: " << *direccion_num << std::endl;
+    std::cout << "Dirección de Memoria: " << direccion_num << std::endl;
+    return 0;
+}
