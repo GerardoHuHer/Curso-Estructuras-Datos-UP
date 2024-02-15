71cfb6809b54b24bc68d705aced2face007edcd8
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2024-02-14 23:21:30 -0600

    Apuntadores

--- Apuntadores/source.cpp
+++ Apuntadores/source.cpp
@@ -2,6 +2,7 @@
 int main() {
     int num = 5;
     int* direccion_num = &num;
+    char letter = 'A';
     std::cout << "Número: " << *direccion_num << std::endl;
     std::cout << "Dirección de Memoria: " << direccion_num << std::endl;
     return 0;
