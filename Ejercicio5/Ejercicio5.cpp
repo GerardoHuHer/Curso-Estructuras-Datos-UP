36a31e7d9dc4fcb05d796c4b9aa2a12ec0804bd0
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2024-02-21 11:16:22 -0600

    Ejercicio 5

--- Ejercicio5/Ejercicio5.cpp
+++ Ejercicio5/Ejercicio5.cpp
@@ -1,1 +1,40 @@
 #include "Ejercicio5.h"
+
+Arreglo::Arreglo(){
+  std::cout << "Enter the number of rows: \n? "; 
+  std::cin >> this->row;
+  std::cout << "Enter the number of columns: \n? "; 
+  std::cin >> this->col;
+  arr = new int*[this->row];
+  for(int i = 0; i < this->row; i++) 
+    *(this->arr + i) = new int[this->col];  
+  namefn();
+}
+
+void Arreglo::namefn(){
+  std::cout << "Enter the name: \n? ";
+  std::getline(std::cin, this->name);
+}
+
+void Arreglo::insert(){
+  int cont{0};
+  int cont2{0};
+  while(*(arr + cont) != nullptr){
+    while(*(*(arr + cont2) + cont) != '\0'){
+      std::cout << "Enter the number "<< cont2 + 1<< " from the row " << cont + 1 << ": \n? ";
+      std::cin >> *(*(arr + cont2) + cont);
+      cont2++;
+    }
+    cont++;
+  }
+}
+
+void Arreglo::show(){
+  for(int i = 0;i < this->row; i++){
+    for(int j = 0; j < this->col; i++){
+      std::cout << "\t" << *(*(arr + j) + i);
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+}
--- Ejercicio5/Ejercicio5.h
+++ Ejercicio5/Ejercicio5.h
@@ -1,2 +1,24 @@
 #pragma once
 #include <iostream>
+#include <string>
+
+/*
+Empleando C++ construya una aplicación que:
+a) Lea un nombre con apellidos, Declare y lea un arreglo de enteros, dinámico de dos dimensiones
+b) Muestre el nombre leído, Empleando apuntadores muestre el arreglo de enteros.
+c) Ordene ascendentemente (de mayor a menor) cada renglón del arreglo y muestre todos los renglones
+*/
+
+class Arreglo{
+public:
+  Arreglo();
+  void show();
+  void sort();
+  void insert();
+  void namefn();
+private:
+  std::string name;
+  int** arr;
+  int row;
+  int col;
+};
