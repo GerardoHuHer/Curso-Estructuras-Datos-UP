056c0cbb11637e2607f1796d23047671a9e3f34a
GerardoHuHer <gerardohuerta1502@gmail.com>
2024-02-28 11:18:19 -0600

    Pilas Dinamicas/Ligadas

--- Pilas/Pilas.cpp
+++ Pilas/Pilas.cpp
@@ -1,2 +1,42 @@
 #include "Pilas.h"
 
+Pila::Pila(){
+  tope = NULL;
+}
+
+int Pila::Consultar(){
+  int num;
+  // Paso 1: Verificar si hay valores en la pila
+  if(tope == NULL)
+    return -1;
+  // Paso 2: Motrar los datos del tope
+  num = tope->dato;
+  return num;
+}
+
+void Pila::Insertar(int nuevo){
+  // Paso 1: Crear nodo
+  nodo = new Entero;
+  // Paso 2: Registrar información
+  nodo->dato = nuevo;
+  // Paso 3: Enlace apuntarlo a tope
+  nodo->anterior = tope;
+  // Paso 4: Apuntar tope al nuevo nodo
+  tope = nodo;
+}
+
+int Pila::Extraer(){
+  Entero *extraido;
+  int num;
+  // Paso 1: Verificar si hay valores en la pila
+  if(tope == NULL)
+    return -1;
+  // Paso 2: Extraer información del tope en extraido
+  extraido = tope;
+  // Paso 3: Descender tope
+  tope = tope->anterior;
+  //Paso 4: Entregar los datos extraídos
+  num = extraido->dato;
+  delete extraido;
+  return num;
+}
--- Pilas/Pilas.h
+++ Pilas/Pilas.h
@@ -17,5 +17,40 @@ b) Extraer el valor de la posición señalada por tope.
 c) Decrementar el valor de tope.
 
 Cosultar(peek)(Consultar los datos de la pila)
+a) Verificar que hay valores en la pila. Si tope es menor que mínimo, la pila está vacía.
+b) Mostrar el valor de la posición señalada por "Tope".
+
+PILAS LIGADAS
+Una forma frecuente de implementar las pilas en la computadora es por medio de arreglos, mismos que cuentan con la característica de consecutividad, sin embargo, no siempre se cuenta con esta posibilidad.
+Cuando no se cuenta con localidades consecutivas de memoria para la implementación de las pilas, se puede recurrir a las "pilas ligadas".
+
+Insertar (push):
+1) Crear un "nodo"
+2) Registrar los datos en el campo de información del nodo.
+3) El campo de enlace del nodo hacerlo igual al "tope".
+4) "tope" apuntarlo al nuevo nodo.
+
+Elminar (pop)
+1) Verificar que hay valores en la pila. Si "tope" apunta a NULL, la pila está vacía.
+2) Extraer información del "tope", igualando "extraído" a "tope".
+3) Descender "tope", igualando "tope" al campo de enlace de "extraido"
+4) Entregar los datos del campo de información de "extraido"
+5) Liberar "extraido"
 */
  
+struct Entero{
+  int dato;
+  Entero* anterior;
+};
+
+class Pila{
+public:
+  Pila();
+  int Consultar();
+  int Extraer();
+  void Insertar(int);
+  void Mostrar();
+private:
+  Entero* tope, *nodo;
+};
+
